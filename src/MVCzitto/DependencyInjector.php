ade27b2c183038d766353773701e0ced0af3e885
Andrei de Oliveira Mosman <andrei.mosman@gmail.com>
2022-06-16 16:40:03 -0300

    Allow functions on DI

--- src/MVCzitto/DependencyInjector.php
+++ src/MVCzitto/DependencyInjector.php
@@ -33,7 +33,7 @@ class DependencyInjector
 
     public function get($key) : mixed
     {
-        return $this->container[$key];
+        return is_callable($this->container[$key]) ? $this->container[$key]() : $this->container[$key];
     }
 
     public function __get($key) : mixed
