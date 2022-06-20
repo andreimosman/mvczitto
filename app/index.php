ed48bc19fb85447130c472b726396ea7d8e44775
Andrei de Oliveira Mosman <andrei.mosman@gmail.com>
2022-06-20 16:57:52 -0300

    Move config to common file

--- app/index.php
+++ app/index.php
@@ -1,20 +1,9 @@
 <?php
 
 require_once("../vendor/autoload.php");
+require_once("config.php");
 
-use MVCzitto\Config\Config;
-use MVCzitto\DependencyInjector;
 use MVCzitto\Application\WebApplication;
-
-define('ENVIRONMENT', 'development');
-ENVIRONMENT == 'development' ?? ini_set('display_errors', 1);
-
-define('APP_PATH', __DIR__);
-$config = Config::loadFromFolder(APP_PATH.'/config');
-$uriFolder = trim( substr(dirname($_SERVER['DOCUMENT_URI']),1) );
-
-$config->app->baseurl = $config->app->baseurl ?: (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . ($uriFolder ? '/' . $uriFolder : '');
-define('BASE_URL', $config->app->baseurl);
 
 $app = WebApplication::getInstance();
 $app->bootstrap();
